7aea475d44d4dc9bcf8cfcc99f6571f39f29d4f6
Nitin Das <37865475+nitinda@users.noreply.github.com>
2021-06-21 22:52:55 +0100

    Created base component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import "./App.css";
+import React from "react";
+// import Button from "@material-ui/core/Button";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+export default App;
